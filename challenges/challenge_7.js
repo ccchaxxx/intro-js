539002f7ff43e308748e046cc3c013ba32bc7ef7
ccchaxxx <27johnschak@washk12.org>
2025-02-27 11:40:58 -0700

    finished challenge 7

--- challenges/challenge_7.js
+++ challenges/challenge_7.js
@@ -1,7 +1,8 @@
 const prompt = require('prompt-sync')();
+ let  lockIsBroken= prompt('is the lock broken?') === 'yes'
 let hasKey = prompt("Do you have a key? (yes/no) ") === "yes";
 let strength = parseInt(prompt("Enter your strength: "));
-if (hasKey && strength >= 8) {
+if (hasKey && strength >= 8 && lockIsBroken != true) {
    console.log("You opened the door!");
 } else {
    console.log("The door won't budge.");
